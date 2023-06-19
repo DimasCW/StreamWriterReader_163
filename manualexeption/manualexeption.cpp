cb1b331f41aa3970d3b948d8876a7feea03a6fe9
DimasCW <cdimas292@gmail.com>
2023-06-19 15:32:47 +0700

    membuat catch adalah blok yang dibuat untuk menerima throw

--- manualexeption/manualexeption.cpp
+++ manualexeption/manualexeption.cpp
@@ -5,5 +5,9 @@ int main() {
 	try {
 		cout << "Selamat belajar di prodi TI" << endl;
 		throw 0.5;//melempar sebuah intger maka
+		cout << "pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+
 	}
 }
