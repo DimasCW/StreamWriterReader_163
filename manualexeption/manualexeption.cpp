0a5a661c34e5771f5a5897216978a96c8f1e3e27
DimasCW <cdimas292@gmail.com>
2023-06-19 15:34:45 +0700

    menyelesaikan manual exeption

--- manualexeption/manualexeption.cpp
+++ manualexeption/manualexeption.cpp
@@ -8,6 +8,12 @@ int main() {
 		cout << "pernyataan tidak akan dieksekusi" << endl;
 	}
 	catch (int a) {
-
+		//blok ini akan disesuaikan 
+		cout << "pengecualian akan dieksekusi" << endl;
 	}
+	catch(...){
+		//jika selain integer maka blok ini akan dieksekusi
+		cout << "default pengecualian dieksekusi" << endl;
+	}
+	return 0;
 }
